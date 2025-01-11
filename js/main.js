4f41841cfa1d51aab5db622644a30772be069b96
Tiago Oliveira <tiago.engsoft@outlook.com>
2025-01-11 20:19:32 -0300

    secondPart

--- js/main.js
+++ js/main.js
@@ -10,23 +10,38 @@ function adjustsSuchAStageGame() {
 
 adjustsSuchAStageGame()
 
-var positionX = Math.floor(Math.random() * width) - 100
-var positionY = Math.floor(Math.random() * heigth) - 100
 
-positionX = positionX < 0 ? 0 : positionX
-positionY = positionY < 0 ? 0 : positionY
-
-console.log(positionX, positionY)
-
-//criar o elemento html
 function positionRandomica() {
+
+    var positionX = Math.floor(Math.random() * width) - 100
+    var positionY = Math.floor(Math.random() * heigth) - 100
+
+    positionX = positionX < 0 ? 0 : positionX
+    positionY = positionY < 0 ? 0 : positionY
+
+    console.log(positionX, positionY)
+
+    //Criar o elemento html
     var insects = document.createElement('img')
     insects.src = 'img/mosca.png'
-    insects.className = 'insect'
+    insects.className = randomSize()
     insects.style.left = positionX + 'px'
     insects.style.top = positionY + 'px'
     insects.style.position = 'absolute'
 
 
     document.body.appendChild(insects)
+}
+
+function randomSize() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'insects'
+        case 1:
+            return 'insectsOne'
+        case 2:
+            return 'insectsTwo'
+    }
 }
